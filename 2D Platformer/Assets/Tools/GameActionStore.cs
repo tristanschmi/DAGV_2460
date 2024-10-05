7b0de7d41cf89654f64ed614791db1e3aabf6263
tristan_s <waitwhat314@gmail.com>
2024-10-04 19:24:28 -0600

    WhiteBox and 2D Unity Project

---
+++ 2D Platformer/Assets/Tools/GameActionStore.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameActionStore : MonoBehaviour
+{
+    public GameAction gameAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
